414ed5a0bccea3e42022b7b506fa45a2f65383d8
Tatang Bukhori <tatangb909@gmail.com>
2025-06-29 17:29:40 +0700

    Belajar Rest API di Express JS

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Route to page
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+
+// import body parser
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
+
+// parse application/json
+app.use(bodyParser.json())
+
+// import route posts
+const postsRouter = require('./routes/posts');
+app.use('/api/posts', postsRouter); //use route posts di Express
+
+app.listen(port, () => {
+    console.log(`app running at http://localhost:${port}`)
+})
